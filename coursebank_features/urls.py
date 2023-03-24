174357b5ffdf7e4fb14ca8a427e447c3948758a6
James Macklean Lusuegro <jamesmacklean27@gmail.com>
2023-03-24 15:32:20 +0800

    changed urlpatterns from `path` to `url`

--- coursebank_features/urls.py
+++ coursebank_features/urls.py
@@ -5,10 +5,12 @@ URLs for coursebank_features.
 # from django.views.generic import TemplateView  # pylint: disable=unused-import
 
 from django.urls import path
-from .views.views import *
+from django.conf.urls import url
+from .views import views
 
 urlpatterns = [
     # TODO: Fill in URL patterns and views here.
     # re_path(r'', TemplateView.as_view(template_name="coursebank_features/base.html")),
-    path('features/', features_dashboard, name='features_dashboard'),
+    # path('features/', views.features_dashboard, name='features_dashboard'),
+    url(r'^features/$', views.features_dashboard, name='features_dashboard'),
 ]
