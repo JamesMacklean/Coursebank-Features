43525feee3410409456cd48335d9ad2c31962be9
James Macklean Lusuegro <jamesmacklean27@gmail.com>
2023-04-02 20:13:43 +0800

    removed views subfolder and moved vies.py with same level of urls.py

--- coursebank_features/urls.py
+++ coursebank_features/urls.py
@@ -5,12 +5,9 @@ URLs for coursebank_features.
 # from django.views.generic import TemplateView  # pylint: disable=unused-import
 
 from django.urls import path
-from django.conf.urls import url
-from .views import views
+from . import views
 
 urlpatterns = [
     # TODO: Fill in URL patterns and views here.
-    # re_path(r'', TemplateView.as_view(template_name="coursebank_features/base.html")),
-    # path('features/', views.features_dashboard, name='features_dashboard'),
-    url(r'^features/$', views.features_dashboard, name='features_dashboard'),
+    path('features/', views.features_dashboard, name='features_dashboard'),
 ]
