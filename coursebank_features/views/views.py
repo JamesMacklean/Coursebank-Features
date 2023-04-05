a146a496c1d0d1407050e7d3e833718e289d6349
James Macklean Lusuegro <jamesmacklean27@gmail.com>
2023-04-05 10:38:23 +0800

    testing request.user.is_authenticated

--- coursebank_features/views/views.py
+++ coursebank_features/views/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 def dashboard(request):
-    return render(request, 'features/dashboard.html')
+    template_name = 'features/dashboard.html'
+    context = {}
+
+    profile = None
+
+    if request.user.is_authenticated:
+        try:
+            # uname = User.objects.get(username=request.user)
+            context['profile'] = request.user
+        except:
+            context['profile'] = profile
+
+    context['profile'] = profile
+    
+    return render(request, template_name, context)
