62d36c65a7e4dbc160691dc9cc4cc8b50bdda6f8
James Macklean Lusuegro <jamesmacklean27@gmail.com>
2023-04-19 23:01:51 +0800

    created CourseTags model

--- coursebank_features/admin.py
+++ coursebank_features/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import CourseOverviewTags
+from .models import CourseTags
 
-admin.site.register(CourseOverviewTags)
+admin.site.register(CourseTags)
--- coursebank_features/models.py
+++ coursebank_features/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from taggit.managers import TaggableManager
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
 
-class CourseOverviewTags(CourseOverview):
+class CourseTags(models.Model):
     
+    course = models.ForeignKey(CourseOverview, on_delete=models.CASCADE)
     tags = TaggableManager()
 
-    
