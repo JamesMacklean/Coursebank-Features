9013f49d559bff36a46ab5a417d99aa13d48f7d7
James Macklean Lusuegro <jamesmacklean27@gmail.com>
2023-04-19 21:32:47 +0800

    added tags for CourseOverview

--- coursebank_features/models.py
+++ coursebank_features/models.py
@@ -1,3 +1,7 @@
-"""
-Database models for coursebank_features.
-"""
+from django.db import models
+from taggit.managers import TaggableManager
+from course_overviews.models import CourseOverview
+
+class CourseOverviewTags(CourseOverview):
+    
+    tags = TaggableManager()
