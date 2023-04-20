98fb348f06892c7c111bd57d8608350eb8fd412c
James Macklean Lusuegro <jamesmacklean27@gmail.com>
2023-04-20 12:43:25 +0800

    renemed CourseTags into CourseTag

--- coursebank_features/models.py
+++ coursebank_features/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from taggit.managers import TaggableManager
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
 
-class CourseTags(models.Model):
+class CourseTag(models.Model):
     
     course = models.ForeignKey(CourseOverview, on_delete=models.CASCADE)
     tags = TaggableManager()
