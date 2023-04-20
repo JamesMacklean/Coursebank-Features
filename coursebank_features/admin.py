e06f84332d6a07217bc634db6c0bb225a4830d0d
James Macklean Lusuegro <jamesmacklean27@gmail.com>
2023-04-20 16:27:44 +0800

    added multiple model classes

--- coursebank_features/admin.py
+++ coursebank_features/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import CourseTag
+from .models import (CourseTopic, CourseSkill, CourseSubTopic, CourseOrganization)
 
-admin.site.register(CourseTag)
+admin.site.register(CourseTopic)
+admin.site.register(CourseSkill)
+admin.site.register(CourseSubTopic)
+admin.site.register(CourseOrganization)
--- coursebank_features/models.py
+++ coursebank_features/models.py
@@ -2,16 +2,46 @@ from django.db import models
 from taggit.managers import TaggableManager
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
 
-class CourseTag(models.Model):
+class CourseTopic(models.Model):
     
     course = models.ForeignKey(
         CourseOverview, on_delete=models.CASCADE
         )
     topics = TaggableManager()
     skills = TaggableManager()
-    others = TaggableManager()
-    organization = TaggableManager()
 
     def __str__(self):
         return self.name
     
+class CourseSkill(models.Model):
+    
+    course = models.ForeignKey(
+        CourseOverview, on_delete=models.CASCADE
+        )
+
+    skills = TaggableManager()
+
+    def __str__(self):
+        return self.name
+
+class CourseSubTopic(models.Model):
+    
+    course = models.ForeignKey(
+        CourseOverview, on_delete=models.CASCADE
+        )
+
+    subtopic = TaggableManager()
+
+    def __str__(self):
+        return self.name
+
+class CourseOrganization(models.Model):
+    
+    course = models.ForeignKey(
+        CourseOverview, on_delete=models.CASCADE
+        )
+
+    organization = TaggableManager()
+    
+    def __str__(self):
+        return self.name
