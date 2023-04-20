4918cc67eedd481d6eab9f35b5cd82175aef1788
James Macklean Lusuegro <jamesmacklean27@gmail.com>
2023-04-21 02:13:52 +0800

    removed .asView

--- coursebank_features/urls.py
+++ coursebank_features/urls.py
@@ -12,9 +12,9 @@ urlpatterns = [
     path('features-dashboard/', views.dashboard, name='dashboard'),
     
     ####################### COURSE TAGS #######################
-    path('create/subtopic', views.SubtopicCreateView.as_view(), name='create_subtopic'),
-    path('create/primarytopic', views.PrimaryTopicCreateView.as_view(), name='create_primarytopic'),
-    path('create/skill', views.SkillCreateView.as_view(), name='create_skill'),
-    path('create/organization', views.OrganizationCreateView.as_view(), name='create_organization'),
+    path('create/subtopic', views.SubtopicCreateView, name='create_subtopic'),
+    path('create/primarytopic', views.PrimaryTopicCreateView, name='create_primarytopic'),
+    path('create/skill', views.SkillCreateView, name='create_skill'),
+    path('create/organization', views.OrganizationCreateView, name='create_organization'),
     ####################### COURSE TAGS #######################
 ]
