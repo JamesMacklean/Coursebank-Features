962a843d7892a49b7c561fbaf9df611ce9743997
James Macklean Lusuegro <jamesmacklean27@gmail.com>
2023-04-20 18:53:25 +0800

    modified admin.py 002

--- coursebank_features/admin.py
+++ coursebank_features/admin.py
@@ -4,6 +4,9 @@ from .models import (CourseTopic, CourseSkill, CourseSubTopic, CourseOrganizatio
 @admin.register(CourseTopic)
 class CourseTopicAdmin (admin.ModelAdmin):
     list_display = ['course', 'primary_topic']
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('topic')
     
     def primary_topic(self, obj):
         return ", ".join(o for o in obj.topic.names())
@@ -11,6 +14,9 @@ class CourseTopicAdmin (admin.ModelAdmin):
 @admin.register(CourseSkill)
 class CourseSkillAdmin (admin.ModelAdmin):
     list_display = ['course', 'skills']
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('skills')
     
     def skills(self, obj):
         return ", ".join(o for o in obj.skills.names())
@@ -18,12 +24,18 @@ class CourseSkillAdmin (admin.ModelAdmin):
 @admin.register(CourseSubTopic)
 class CourseSubTopicAdmin (admin.ModelAdmin):
     list_display = ['course', 'subtopics']
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('subtopics')
     
     def subtopics(self, obj):
         return ", ".join(o for o in obj.subtopics.names())
 @admin.register(CourseOrganization)
 class CourseOrganizationAdmin (admin.ModelAdmin):
     list_display = ['course', 'organization']
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('organization')
     
     def organization(self, obj):
         return ", ".join(o for o in obj.organization.names())
