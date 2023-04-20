ea390d64556268a148c411fb3931a5549aad88a1
James Macklean Lusuegro <jamesmacklean27@gmail.com>
2023-04-21 01:51:49 +0800

    fixed import

--- coursebank_features/views/views.py
+++ coursebank_features/views/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.views.generic import CreateView, ListView
 from django.urls import reverse_lazy
-from models import *
+from coursebank_features.models import *
 
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import user_passes_test
