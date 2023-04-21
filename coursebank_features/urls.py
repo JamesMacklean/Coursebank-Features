766a02e73ecbeb75e2278f2e2cf4e830c6490520
James Macklean Lusuegro <jamesmacklean27@gmail.com>
2023-04-21 18:11:43 +0800

    Fixed 'add_primary_topic' is not a valid view function or pattern name 002

--- coursebank_features/urls.py
+++ coursebank_features/urls.py
@@ -5,17 +5,17 @@ URLs for coursebank_features.
 # from django.views.generic import TemplateView  # pylint: disable=unused-import
 
 from django.urls import path
-from .views.views import *
+from .views import views
 
 urlpatterns = [
-    path('features/', main, name='main'),
+    path('features/', views.main, name='main'),
     
     ####################### COURSE TAGS #######################
-    path('course_tag/', course_tag, name='course_tag'),
-    path('course_tag/add/', add_course_tag, name='add_course_tag'),
-    path('course_tag/add/primary_topic/', add_primary_topic, name='add_primary_topic'),
-    path('course_tag/add/subtopic/', add_subtopic, name='add_subtopic'),
-    path('course_tag/add/skill/', add_skill, name='add_skill'),
-    path('course_tag/add/organization/', add_organization, name='add_organization'),
+    path('course_tag/', views.course_tag, name='course_tag'),
+    path('course_tag/add/', views.add_course_tag, name='add_course_tag'),
+    path('course_tag/add/primary_topic/', views.add_primary_topic, name='add_primary_topic'),
+    path('course_tag/add/subtopic/', views.add_subtopic, name='add_subtopic'),
+    path('course_tag/add/skill/', views.add_skill, name='add_skill'),
+    path('course_tag/add/organization/', views.add_organization, name='add_organization'),
     ####################### COURSE TAGS #######################
 ]
