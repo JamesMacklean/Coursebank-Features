4f9d5c8f1551b779aef3190ed113876a89025351
James Macklean Lusuegro <jamesmacklean27@gmail.com>
2023-04-22 17:56:09 +0800

    fixed 'CourseOverview' object has no attribute 'course_id'.

--- coursebank_features/api/serializers.py
+++ coursebank_features/api/serializers.py
@@ -1,7 +1,14 @@
+from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
+
 from rest_framework import serializers
 from coursebank_features.models import *
 
 ####################### COURSE TAGS #######################
+class CourseOverviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CourseOverview
+        fields = ('id', 'display_name')
+
 class PrimaryTopicSerializer(serializers.ModelSerializer):
     class Meta:
         model = PrimaryTopic
@@ -23,8 +30,8 @@ class OrganizationSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class CourseTagSerializer(serializers.ModelSerializer):
-    course_id = serializers.UUIDField(source='course.course_id')
-    course_display_name = serializers.CharField(source='course.display_name')
+    course_id = serializers.SerializerMethodField()
+    course_display_name = serializers.SerializerMethodField() 
     primary_topic = PrimaryTopicSerializer()
     subtopic = SubTopicSerializer(many=True)
     skills = SkillSerializer(many=True)
@@ -32,4 +39,19 @@ class CourseTagSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CourseTag
-        fields = ['id','course_id', 'course_display_name', 'primary_topic', 'subtopic', 'skills', 'organization']
+        fields = ['id', 'course_id', 'course_display_name', 'primary_topic', 'subtopic', 'skills', 'organization']
+        
+    def get_course_id(self, obj):
+        course_overview = obj.course_overview
+        if course_overview is not None:
+            return CourseOverviewSerializer(course_overview).data['id']
+        else:
+            return obj.id
+        
+    def get_course_display_name(self, obj):
+        course_overview = obj.course_overview
+        if course_overview is not None:
+            return course_overview.display_name
+        else:
+            return None
+        
