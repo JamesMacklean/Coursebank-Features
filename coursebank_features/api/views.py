dc1dae254fa099b1d6c268e0cb1259977837f0f9
James Macklean Lusuegro <jamesmacklean27@gmail.com>
2023-04-22 12:22:04 +0800

    removed decorator for api

--- coursebank_features/api/views.py
+++ coursebank_features/api/views.py
@@ -1,12 +1,10 @@
 from rest_framework import viewsets
-from django.contrib.admin.views.decorators import staff_member_required
 
 from coursebank_features.api.serializers import *
 from coursebank_features.models import *
 
 
 ####################### COURSE TAGS #######################
-@staff_member_required
 class CourseTagViewSet(viewsets.ModelViewSet):
     queryset = CourseTag.objects.all()
     serializer_class = CourseTagSerializer 
