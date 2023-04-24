8d4c419dbdc9e4a1dce30777062157e9c5d1a00b
James Macklean Lusuegro <jamesmacklean27@gmail.com>
2023-04-24 11:59:49 +0800

    recreated CourseTagAPI

--- coursebank_features/api/urls.py
+++ coursebank_features/api/urls.py
@@ -2,10 +2,11 @@ from django.urls import path, include
 from rest_framework import routers
 from coursebank_features.api.views import *
 
-router = routers.DefaultRouter()
-router.register(r'course-tag', CourseTagViewSet)
+# router = routers.DefaultRouter()
+# router.register(r'course-tag', CourseTagViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('course-tag/', CourseTagAPIView.as_view(), name='course-tag'),
 ]
--- coursebank_features/api/views.py
+++ coursebank_features/api/views.py
@@ -1,9 +1,16 @@
-from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from coursebank_features.api.serializers import *
 from coursebank_features.models import *
 
 ####################### COURSE TAGS #######################
-class CourseTagViewSet(viewsets.ModelViewSet):
-    queryset = CourseTag.objects.all()
-    serializer_class = CourseTagSerializer 
+# class CourseTagViewSet(viewsets.ModelViewSet):
+#     queryset = CourseTag.objects.all()
+#     serializer_class = CourseTagSerializer 
+    
+class CourseTagAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        course_tags = CourseTag.objects.all()
+        serializer = CourseTagSerializer(course_tags, many=True)
+        return Response(serializer.data)
