c36fca1244f97e2a843fcd6ba4f51cafeb9520e8
James Macklean Lusuegro <jamesmacklean27@gmail.com>
2023-04-25 13:13:08 +0800

    cleaned code

--- coursebank_features/api/views.py
+++ coursebank_features/api/views.py
@@ -4,11 +4,6 @@ from rest_framework.response import Response
 from coursebank_features.api.serializers import *
 from coursebank_features.models import *
 
-####################### COURSE TAGS #######################
-# class CourseTagViewSet(viewsets.ModelViewSet):
-#     queryset = CourseTag.objects.all()
-#     serializer_class = CourseTagSerializer 
-    
 class CourseTagAPIView(APIView):
     def get(self, request, *args, **kwargs):
         course_tags = CourseTag.objects.all()
