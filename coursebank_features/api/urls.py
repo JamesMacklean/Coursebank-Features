704231a6cd5dc7d8d9eaa31090c655d399044d5e
avsongco <markallensongco@gmail.com>
2023-05-12 00:10:08 +0800

    edited url pattern for course-bundle api

--- coursebank_features/api/urls.py
+++ coursebank_features/api/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('course-tag/', CourseTagAPIView.as_view(), name='course-tag'),
     path('most-popular-courses/', MostPopularCoursesList.as_view(), name='most-popular-courses'),
-    path('api/course-bundles/', CourseBundleListAPIView.as_view(), name='course_bundle_list'),
+    path('course-bundles/', CourseBundleListAPIView.as_view(), name='course_bundle_list'),
 ]
