efad8e7ab9d4a2acca36eb1cfb2228fc8b1f9706
James Macklean Lusuegro <jamesmacklean27@gmail.com>
2023-05-12 21:52:18 +0800

    created paths for free and latest courses API

--- coursebank_features/api/urls.py
+++ coursebank_features/api/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('course-tag/', CourseTagAPIView.as_view(), name='course-tag'),
     path('courses/most-popular/', MostPopularCoursesAPIView.as_view(), name='most-popular-courses'),
     path('courses/trending/', MostPopularCoursesAPIView.as_view(), name='trending-courses'),
+    path('courses/free/', FreeCoursesAPIView.as_view(), name='free-courses'),
+    path('courses/latest/', LatestCoursesAPIView.as_view(), name='latest-courses'),
 
 ]
