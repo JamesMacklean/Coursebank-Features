3a0110a664a6eea851b929bb9b1d9e8ab463519b
James Macklean Lusuegro <jamesmacklean27@gmail.com>
2023-05-23 16:23:20 +0800

    print('###############',enrollment_count)

--- coursebank_features/api/views.py
+++ coursebank_features/api/views.py
@@ -73,9 +73,9 @@ class TrendingCoursesAPIView(APIView):
             trending_courses = []
             course_enrolled_ids = [course['course_id'] for course in courses_enrolled]
             course_overviews = CourseOverview.objects.filter(id__in=course_enrolled_ids)
-            print(course_overviews)
             for course_overview in course_overviews:
                 enrollment_count = enrollments.get(course_overview.id, 0)
+                print('###############',enrollment_count)
                 if enrollment_count:
                     trending_courses.append({
                         'course_id': course_overview.id,
