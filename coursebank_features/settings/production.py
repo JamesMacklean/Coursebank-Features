2d66db14011ffecf41d4cddfba7c84b26cca3eb9
James Macklean Lusuegro <jamesmacklean27@gmail.com>
2023-06-05 20:11:10 +0800

    settings.CORS_ALLOWED_ORIGINS

--- coursebank_features/settings/production.py
+++ coursebank_features/settings/production.py
@@ -3,3 +3,8 @@ def plugin_settings(settings):
     # For example:
         settings.FEATURES['ENABLE_MY_APP'] = True
         settings.MY_APP_POLICY = 'foo'
+        settings.CORS_ALLOWED_ORIGINS = [
+            # Add the allowed origins (domains) from which requests are allowed
+            # Example: 'https://example.com'
+            '*',
+        ]
