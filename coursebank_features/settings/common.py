bcde417d310489d66ceaf2f1ad5b719c498a7cb9
James Macklean Lusuegro <jamesmacklean27@gmail.com>
2023-06-06 12:20:39 +0800

    reverted

--- coursebank_features/settings/common.py
+++ coursebank_features/settings/common.py
@@ -3,5 +3,3 @@ def plugin_settings(settings):
     # For example:
         settings.FEATURES['ENABLE_MY_APP'] = True
         settings.MY_APP_POLICY = 'foo'
-        settings.CORS_ORIGIN_ALLOW_ALL = True
-        settings.CORS_ALLOW_CREDENTIALS = False
