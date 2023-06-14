407827f8689c5aa5cb9491a53ab89e787ddced21
James Macklean Lusuegro <jamesmacklean27@gmail.com>
2023-06-14 21:12:37 +0800

    settings.CORS_ALLOWED_ORIGINS = ['https://tmtg-clone.click/']

--- coursebank_features/settings/common.py
+++ coursebank_features/settings/common.py
@@ -1,13 +1,13 @@
 def plugin_settings(settings):
     # Enable CORS headers
-    settings.CORS_ALLOW_ALL_ORIGINS = True
+    settings.CORS_ALLOW_ALL_ORIGINS = False
 
     # Add 'corsheaders' middleware
     settings.MIDDLEWARE.insert(0, 'corsheaders.middleware.CorsMiddleware')
 
     # Optional: Customize CORS headers if needed
     settings.CORS_ALLOW_CREDENTIALS = True
-    # settings.CORS_ALLOWED_ORIGINS = ['*']
+    settings.CORS_ALLOWED_ORIGINS = ['https://tmtg-clone.click/']
     settings.CORS_ALLOWED_METHODS = ['GET', 'POST']
     settings.CORS_ALLOWED_HEADERS = ['*']
 
