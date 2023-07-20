53ad8ffaba6985e58aa6a5b65fd31cd3a022cf85
avsongco <markallensongco@gmail.com>
2023-07-20 20:05:08 +0800

    reconfigured utils and exp learner profiles

--- coursebank_features/management/commands/export_learner_profiles.py
+++ coursebank_features/management/commands/export_learner_profiles.py
@@ -15,31 +15,17 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            '-a',
-            '--isactive',
-            type=str,
-            help='set filter for active users',
-        )
-        parser.add_argument(
             '-e',
             '--email',
             type=str,
             help='set email to send to',
         )
-        parser.add_argument(
-            '-c',
-            '--course',
-            type=str,
-            help='set filter for course_id',
-        )
 
     def handle(self, *args, **options):
-        course_id = options.get('course', None)
         email_address = options.get('email', None)
-        active = options.get('isactive',None)
 
         try:
-            export_learner_profiles(active, course_id, email_address=email_address)
+            export_learner_profiles(email_address=email_address)
         except Exception as e:
             raise CommandError("Error in exporting learner profiles: {}".format(str(e)))
         else:
