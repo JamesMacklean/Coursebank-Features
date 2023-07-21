0ecfc1537ba5060582d77312d9ca4c212a4ae9a0
avsongco <markallensongco@gmail.com>
2023-07-21 16:26:24 +0800

    added smtp to command

--- coursebank_features/management/commands/export_learner_profiles.py
+++ coursebank_features/management/commands/export_learner_profiles.py
@@ -1,9 +1,11 @@
 import csv
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
-from django.core.mail import EmailMessage
-from django.conf import settings
-from io import StringIO
 
 class Command(BaseCommand):
     help = 'Export data from Django User model to a CSV file and send it via email'
@@ -20,29 +22,54 @@ class Command(BaseCommand):
 
         queryset = User.objects.all()
 
-        # Prepare the CSV data as a string
-        csv_data = StringIO()
-        writer = csv.writer(csv_data)
-        writer.writerow(['id', 'name', 'username', 'date_joined', 'is_active'])
-
+        # Prepare the CSV data as a list of dictionaries
+        csv_data = []
         for user in queryset:
-            writer.writerow([user.id, user.get_full_name(), user.username, user.date_joined, user.is_active])
+            csv_data.append({
+                'id': user.id,
+                'name': user.get_full_name(),
+                'username': user.username,
+                'date_joined': user.date_joined,
+                'is_active': user.is_active,
+            })
 
-        # Create the email message with CSV attachment
-        subject = 'Exported Coursebank Learner Profiles'
-        body = 'Please find the attached CSV file containing the learner profiles'
-        sender_email = 'no-reply-coursebank-learner-profiles@coursebank.ph'
+        # Create a CSV file in-memory
+        csv_file_path = 'exported_data.csv'
+        with open(csv_file_path, 'w', newline='') as f:
+            fieldnames = csv_data[0].keys()
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(csv_data)
 
-        email = EmailMessage(
-            subject=subject,
-            body=body,
-            from_email=sender_email,
-            to=[recipient_email],
-        )
+        # Send the email with the CSV attachment
+        subject = 'Exported Data from Django User Model'
+        body = 'Please find the attached CSV file containing the data.'
+        sender_email = 'sender@example.com'  # Replace with the sender email address
 
-        email.attach('learner_profiles.csv', csv_data.getvalue(), 'text/csv')
+        msg = MIMEMultipart()
+        msg['From'] = sender_email
+        msg['To'] = recipient_email
+        msg['Subject'] = subject
 
-        # Send the email
-        email.send()
+        msg.attach(MIMEText(body, 'plain'))
 
-        self.stdout.write(self.style.SUCCESS(f'Learner profiles data exported and sent to {recipient_email} successfully'))
+        # Attach the CSV file to the email
+        with open(csv_file_path, 'rb') as file:
+            attachment = MIMEBase('application', 'octet-stream')
+            attachment.set_payload(file.read())
+            encoders.encode_base64(attachment)
+            attachment.add_header('Content-Disposition', f'attachment; filename={csv_file_path}')
+            msg.attach(attachment)
+
+        # Connect to SMTP server and send the email
+        smtp_server = 'smtp.sendgrid.net'
+        smtp_port = 587 
+        smtp_username = 'apikey'
+        smtp_password = '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'  # Replace with your SMTP password
+
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()
+            server.login(smtp_username, smtp_password)
+            server.sendmail(sender_email, recipient_email, msg.as_string())
+
+        self.stdout.write(self.style.SUCCESS(f'Data exported and sent to {recipient_email} successfully'))
